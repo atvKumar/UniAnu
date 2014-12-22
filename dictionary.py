5692a78c8b69acfbab48b6271aa6e3d28362ad9c
Kumaran <atv.kumar@gmail.com>
2014-12-22 18:29:01 +0530

    Update TamilCube Dictionary
    Optimised for less calls and more efficient webpage calls

--- dictionary.py
+++ dictionary.py
@@ -1,37 +1,61 @@
 from urllib import urlencode
 from urllib2 import Request, urlopen
 
-def getVIEWSTATE(search_term="id=\"__VIEWSTATE\" value=\"/",
-                 url="http://dictionary.tamilcube.com/index.aspx"):
-    response = urlopen(url)
-    data = [x for x in response if search_term in x]
-    return data[0].split('"')[7]
+_url = "http://dictionary.tamilcube.com/index.aspx"
+_data = ''
+_viewstate = ''
+_eventvalidation = ''
 
-def getEVENTVALIDATION(search_term="id=\"__EVENTVALIDATION\" value=\"/",
-                       url="http://dictionary.tamilcube.com/index.aspx"):
-    response = urlopen(url)
-    data = [x for x in response if search_term in x]
-    return data[0].split('"')[7]
 
-def TamilCube_Eng2Tm(search_term,
-                     url="http://dictionary.tamilcube.com/index.aspx"):
-    # Dummy header to bypass blocks and bots
-    headers = {
+def getTamilCube_Data(refresh=False):
+    global _data
+    if _data == '' or refresh:
+        _data = getWebPage(_url)
+        setTamilCube_id()
+
+
+def getHeaders():
+    return {
     'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; '
                        'rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
     'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml; q=0.9,'
                    '*/*; q=0.8',
-    'Content-Type': 'application/x-www-form-urlencoded'}
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-    #Tamil Cube Form Fields
+
+def getWebPage(url):
+    request = Request(url, headers=getHeaders())
+    response = urlopen(request)
+    if response.getcode() == 200:
+        return response.read()
+
+
+def setTamilCube_id():
+    global _viewstate, _eventvalidation
+    if _viewstate == '' and _eventvalidation == '':
+        viewstate_list = list()
+        eventvalidation_list = list()
+        for line in _data.split('\n'):
+            if "id=\"__VIEWSTATE\" value=\"/" in line:
+                viewstate_list.append(line)
+            if "id=\"__EVENTVALIDATION\" value=\"/" in line:
+                eventvalidation_list.append(line)
+        _viewstate = viewstate_list[0].split('"')[7]
+        _eventvalidation = eventvalidation_list[0].split('"')[7]
+
+
+def getTamilCube_FormData(search_term):
     formFields = ((r'__EVENTTARGET', r''), (r'__EVENTARGUMENT', r''),
-                  (r'__VIEWSTATE', getVIEWSTATE()),
-                  (r'__EVENTVALIDATION', getEVENTVALIDATION()),
+                  (r'__VIEWSTATE', _viewstate),
+                  (r'__EVENTVALIDATION', _eventvalidation),
                   (r'name', search_term), (r'Submit1', r'Search'))
+    return urlencode(formFields)
 
-    encodedFields = urlencode(formFields)
 
-    request = Request(url, encodedFields, headers)
+def TamilCube_Eng2Tm(search_term):
+    encodedFields = getTamilCube_FormData(search_term)
+    request = Request(_url, encodedFields, getHeaders())
     response= urlopen(request)
 
     for line in response:
@@ -41,5 +65,5 @@ def TamilCube_Eng2Tm(search_term,
                 #print line.strip().split('.<')[0]
                 yield line.strip().split('.<')[0]
 
-# for x in TamilCube_Eng2Tm('water'):
-#     print x
+
+getTamilCube_Data()
